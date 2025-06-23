2b14d933ea7ab1686fb18aa1c66d35b81aaf8b16
stanloky450 <backlinkreator@gmail.com>
2025-06-23 16:45:34 +0100

    first commit

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
